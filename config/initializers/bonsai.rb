ca10a6005f5f68d7c97756db36ced514b4c5fde5
Geeh <rangel.soares.souza@gmail.com>
2016-07-12 15:53:40 -0300

    elastic search heroku

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,1 +1,3 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['https://3u785wmp:c9xe4emd34pxoofr@ivy-9550028.us-east-1.bonsai.io']
+if ENV['https://3u785wmp:c9xe4emd34pxoofr@ivy-9550028.us-east-1.bonsai.io']
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['https://3u785wmp:c9xe4emd34pxoofr@ivy-9550028.us-east-1.bonsai.io'], logs: true})
+end
