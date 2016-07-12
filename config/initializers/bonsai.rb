3e26d1240041fcfa4c999d9048a7dc01e3704a6e
Geeh <rangel.soares.souza@gmail.com>
2016-07-12 16:07:09 -0300

    elastich search heroku

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,3 @@
-if ENV['https://3u785wmp:c9xe4emd34pxoofr@ivy-9550028.us-east-1.bonsai.io']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['https://3u785wmp:c9xe4emd34pxoofr@ivy-9550028.us-east-1.bonsai.io'], logs: true})
+if ENV['heroku config:set BONSAI_URL=https://8vjyyjjpf8:ameu48pruh@rangels-first-sandbo-2302849557.us-east-1.bonsai.io']
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['heroku config:set BONSAI_URL=https://8vjyyjjpf8:ameu48pruh@rangels-first-sandbo-2302849557.us-east-1.bonsai.io'], logs: true})
 end
